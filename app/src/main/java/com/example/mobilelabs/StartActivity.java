42bf1b0a23a29442e27588e92ca53055cd1d4bb1
ktrepfk <ktrepfk@mail.ru>
2022-11-26 13:17:24 +0400

    lab4-5

--- app/src/main/java/com/example/mobilelabs/StartActivity.java
+++ app/src/main/java/com/example/mobilelabs/StartActivity.java
@@ -3,23 +3,41 @@ package com.example.mobilelabs;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
-public class StartActivity extends AppCompatActivity {
+import java.util.Random;
 
+public class StartActivity extends AppCompatActivity {
+    int clickCount = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
 
+        ImageButton btn = findViewById(R.id.imageButtonStartClick);
+
+        btn.setOnClickListener(v -> {
+            clickCount += 1;
+            int width = Resources.getSystem().getDisplayMetrics().widthPixels;
+            int height = Resources.getSystem().getDisplayMetrics().heightPixels;
+            Random r = new Random();
+            btn.setX(r.nextInt(width - 170));
+            btn.setY(r.nextInt(height - 400));
+            if (clickCount == 10){
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
         View img = findViewById(R.id.imageViewStartActivity);
         img.setOnClickListener(v -> {
-            Toast.makeText(this, "Хрю", Toast.LENGTH_SHORT).show();
-            Intent intent = new Intent(this, MainActivity.class);
-            startActivity(intent);
-            finish();
+            Toast.makeText(this, clickCount + "/10", Toast.LENGTH_SHORT).show();
         });
     }
 }
